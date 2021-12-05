0a0108aacecb2d29f241c28ffa75ac37e38e2d9e
Lucía Oderiz <miluioz@gmail.com>
2021-12-05 17:24:52 -0300

    cambios en scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/map-earth.png');{
-      myImage.setAttribute('src','images/peony-flowers.png');
-    } else {
-      myImage.setAttribute('src','images/map-earth.png');
-    }
-}
-
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/map-earth.png') {
+    myImage.setAttribute('src','images/peony-flowers.png');
+  } else {
+    myImage.setAttribute('src','images/map-earth.png');
+  }
+};
