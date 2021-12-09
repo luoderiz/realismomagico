d6b5a8dd9b1c6d91a9d77fcab0a3a200bd2b4393
Lucía Oderiz <miluioz@gmail.com>
2021-12-08 21:08:35 -0300

    Added complaint button for data entry

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-let myImage = document.querySelector('img');
-//image changer
+let myImage = document.querySelector('#portada');
+
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if (mySrc === 'images/map-earth.png') {
@@ -10,7 +10,7 @@ myImage.onclick = function() {
 }
 
 //personalized greeting
-let myButton = document.querySelector('button');
+
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
@@ -30,28 +30,35 @@ if(!localStorage.getItem('name')) {
   myHeading.textContent = 'Es nuestra cultura, ' + storedName;
 }
 
-myButton.onclick = function() {
+nameChanger.onclick = function() {
   setUserName();
 }
 
 //dinamic list
-const list = document.createElement('ul');
+const newList = document.createElement('ul');
+newList.id='listaDenuncias'
 const info = document.createElement('p');
-const html = document.querySelector('html');
+info.id='itemDenuncia'
 
-info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+let denButton = document.querySelector('#denButton');
 
+info.textContent = 'Por ahora tenemos pocos';
 document.body.appendChild(info);
-document.body.appendChild(list);
+document.body.appendChild(newList);
 
-html.onclick = function() {
+function insertDenuncia() {
   const listItem = document.createElement('li');
-  const listContent = prompt('What content do you want the list item to have?');
+  const listContent = prompt('Inserte aquí el robo cultural que desear denunciar');
   listItem.textContent = listContent;
-  list.appendChild(listItem);
-  listItem.onclick = function(e) {
-    e.stopPropagation();
-    const listContent = prompt('Enter new content for your list item');
-    this.textContent = listContent;
+  for (let item of newList.childNodes) { 
+  if (listItem.textContent === item.textContent) {
+    alert('Gracias, lector. Su compromiso nos hace grandes. Igual, podría ser más atento, su denuncia ya existe');
+    return
+    }
   }
+  newList.appendChild(listItem);
+}
+
+denButton.onclick = function() {
+  insertDenuncia();
 }
